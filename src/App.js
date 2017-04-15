86fc10b5b6c796a9284eab6d7a0877e55e917cad
Resdan Logan <rezn5447@gmail.com>
2017-04-14 17:24:56 -0700

    created images and basic set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './assets/logo.svg';
 import Parallax from 'react-springy-parallax'
 import imagey from './assets/clients-main.svg'
 import pBG from './assets/parallaxBackground.png'
+import BG from './assets/BG1.svg'
+import Pic from './assets/Picture1.jpg'
+import INAB_Grp from './assets/INAB_Group1.svg'
+
+
 import './App.css';
 
 class App extends Component {
@@ -11,7 +16,6 @@ class App extends Component {
     parallax:{
       backgroundImage: `url(${pBG})`,
       backgroundSize: '100%'
-
       },
     parallaxLayer: {
        fontFamily: 'Menlo-Regular, Menlo, monospace',
@@ -23,13 +27,28 @@ class App extends Component {
      innerDiv: {
        backgroundColor: 'yellow',
        height: '80%',
-       width: '80%'
+       width: '90%',
+       display: 'flex', alignItems: 'center', justifyContent: 'space-between'
      },
-     textDiv: {
-
+     picImg: {
+       borderRadius: '55px'
      },
-     screenShotDiv: {
+     sectionDiv: {
+       display: 'flex',
 
+       width: '45%',
+       flexDirection: 'column',
+       alignItems: 'center',
+       justifyContent:'center'
+     },
+     skillsDiv: {
+       display: 'flex',
+       flexDirection: 'row',
+       alignItems: 'center',
+       justifyContent:'center'
+     },
+     gridDiv: {
+       margin: '5px'
      }
   }
     return (
@@ -40,14 +59,57 @@ class App extends Component {
           <Parallax.Layer offset={2} speed={1} style={{ backgroundColor: '#0F4C5C' }} />
           <Parallax.Layer offset={3} speed={1} style={{ backgroundColor: '#DE3645' }} />
 
-
           <Parallax.Layer
               offset={0}
               speed={0.5}
               style={styles.parallaxLayer}
               onClick={() => this.refs.parallax.scrollTo(1)}>
-              <div style={styles.innerDiv}>
-                Writing Stuff here
+              <div style={{...styles.innerDiv, backgroundImage: `(uri:${BG})`}}>
+                <div style={styles.sectionDiv}>
+                  <h1>Resdan Logan</h1>
+                  <img src={Pic} style={styles.picImg} className="pic1" alt="My-Picture"/>
+                </div>
+                <div style={styles.sectionDiv}>
+                  <h2>About Me:</h2>
+
+                </div>
+                <div style={styles.sectionDiv}>
+                  <h2>Skills:</h2>
+                  <div style={styles.skillsDiv}>
+                    <div style={styles.gridDiv}>
+                      <p>Technolofdafdfasgies:</p>
+                    </div>
+                    <div  style={styles.gridDiv}>
+                      <p>React</p>
+                      <p>Redux</p>
+                      <p>React Native</p>
+                      <p>React Native</p>
+                    </div>
+                    <div  style={styles.gridDiv}>
+                      <p>Ruby</p>
+                      <p>AJAX</p>
+                      <p>JQuery</p>
+                      <p>SCSS / SASS </p>
+                    </div>
+                  </div>
+                  <div style={styles.skillsDiv}>
+                    <div style={styles.gridDiv}>
+                      <p>Tools and Softwares:</p>
+                    </div>
+                    <div  style={styles.gridDiv}>
+                      <p>RESTful APIs</p>
+                      <p>Adobe Creative Suite</p>
+                      <p>Firebase</p>
+                      <p>Xcode</p>
+                    </div>
+                    <div  style={styles.gridDiv}>
+                      <p>Trello</p>
+                      <p>SnapKit</p>
+                      <p>GitHub</p>
+                      <p>Postgres</p>
+                    </div>
+                  </div>
+                </div>
               </div>
           </Parallax.Layer>
 
@@ -56,8 +118,10 @@ class App extends Component {
               speed={-0.1}
               style={styles.parallaxLayer}
               onClick={() => this.refs.parallax.scrollTo(2)}>
-              <div style={styles.innerDiv}>
-                Writing Stuff here
+              <div style={{...styles.innerDiv, backgroundImage: `url(${BG})`, backgroundPosition: 'fixed', backgroundSize: '100% 100%'}}>
+                <div></div>
+                <div></div>
+                <div></div>
               </div>
           </Parallax.Layer>
 
